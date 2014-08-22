58d3bec4111271740501f60755853fb22e683a76
MagicalLilicorn <lily4ever@msn.com>
2014-08-22 11:16:52 -0700

    Fixed stuffs

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -11,18 +11,18 @@ void draw()
 	x = random(0,100);
 	drawCircles();
 	drawRect();
-	image(img, random(0,1400),random(-100,500));
+	image(img, random(-500,1400),random(-500,500));
 }
 
 void drawCircles() 
 {
 	noStroke();
 	fill(random(212,255),random(28,100),28);
-	ellipse(random(0,1400),random(-100,500),x+50,x+50);
+	ellipse(random(0,1400),random(-200,500),x+50,x+50);
 }
 
 void drawRect() {
 	noStroke();
 	fill(random(212,255),random(206,255),random(28,75));
-	rect(random(0,1400), random(-100,500),x+50,x+50);
+	rect(random(-200,1400), random(-200,500),x+50,x+50);
 }
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -27,25 +27,20 @@ public void draw()
 	x = random(0,100);
 	drawCircles();
 	drawRect();
-	image(img, random(0,1400),random(0,500));
+	image(img, random(-500,1400),random(-500,500));
 }
 
 public void drawCircles() 
 {
 	noStroke();
 	fill(random(212,255),random(28,100),28);
-	ellipse(random(0,1400),random(0,500),x+50,x+50);
+	ellipse(random(0,1400),random(-200,500),x+50,x+50);
 }
 
 public void drawRect() {
 	noStroke();
 	fill(random(212,255),random(206,255),random(28,75));
-	rect(random(0,1400), random(0,500),x+50,x+50);
-}
-
-public void pika(){
-	fill(0,0,0);
-	text("pika pika", random(0,1400),random(0,500));
+	rect(random(-200,1400), random(-200,500),x+50,x+50);
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
