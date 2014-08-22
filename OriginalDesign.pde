dcf535785fdfeaa710ca56cbffa6b221987bc595
MagicalLilicorn <lily4ever@msn.com>
2014-08-22 11:13:20 -0700

    PIKACHUUUUUU

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,7 +1,8 @@
 float x;
-
+PImage img;
 void setup()
 {
+	img = loadImage("pikachu.gif.gif");
 	size(1400,500);
 	background(200,200,200);
 }
@@ -10,18 +11,23 @@ void draw()
 	x = random(0,100);
 	drawCircles();
 	drawRect();
+	image(img, random(0,1400),random(0,500));
 }
 
 void drawCircles() 
 {
 	noStroke();
-	fill(random(74,200),random(162,200),224);
+	fill(random(212,255),random(28,100),28);
 	ellipse(random(0,1400),random(0,500),x+50,x+50);
 }
 
 void drawRect() {
 	noStroke();
-	fill(random(53,150),random(242,255),random(56,75));
+	fill(random(212,255),random(206,255),random(28,75));
 	rect(random(0,1400), random(0,500),x+50,x+50);
 }
 
+void pika(){
+	fill(0,0,0);
+	text("pika pika", random(0,1400),random(0,500));
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -15,10 +15,11 @@ import java.io.IOException;
 public class OriginalDesign extends PApplet {
 
 float x;
-
+PImage img;
 public void setup()
 {
-	size(1600,500);
+	img = loadImage("pikachu.gif.gif");
+	size(1400,500);
 	background(200,200,200);
 }
 public void draw()
@@ -26,21 +27,26 @@ public void draw()
 	x = random(0,100);
 	drawCircles();
 	drawRect();
+	image(img, random(0,1400),random(0,500));
 }
 
 public void drawCircles() 
 {
 	noStroke();
-	fill(random(74,200),random(162,200),224);
-	ellipse(random(50,900),random(50,500),x+50,x+50);
+	fill(random(212,255),random(28,100),28);
+	ellipse(random(0,1400),random(0,500),x+50,x+50);
 }
 
 public void drawRect() {
 	noStroke();
-	fill(random(53,150),random(242,255),random(56,75));
-	rect(random(50,900), random(50,500),x+50,x+50);
+	fill(random(212,255),random(206,255),random(28,75));
+	rect(random(0,1400), random(0,500),x+50,x+50);
 }
 
+public void pika(){
+	fill(0,0,0);
+	text("pika pika", random(0,1400),random(0,500));
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
